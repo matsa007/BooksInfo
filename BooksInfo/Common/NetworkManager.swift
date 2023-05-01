5287a119506a5acd7c273d3a68e2c402e142f893
matsa_007 <serg.arbitran@gmail.com>
2023-05-02 00:52:20 +0300

    Network manager and URL for API call added

---
+++ BooksInfo/Common/NetworkManager.swift
@@ -0,0 +1,75 @@
+//
+//  NetworkManager.swift
+//  BooksInfo
+//
+//  Created by Сергей Матвеенко on 2.05.23.
+//
+
+import Foundation
+
+class NetworkManager {
+    static var shared = NetworkManager()
+    
+    func makeRequest<T: Codable>(to endpoint: String, completion: @escaping (Result<T, Error>) -> ()) {
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(NetworkError.invalidResponse))
+                return
+            }
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NetworkError.statusCode(httpResponse.statusCode)))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let decodedData = try decoder.decode(T.self, from: data!)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    func getImageData(from urlString: String, completion: @escaping (Result<Data, Error>) -> ()) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(NetworkError.invalidResponse))
+                return
+            }
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NetworkError.statusCode(httpResponse.statusCode)))
+                return
+            }
+            guard let data = data else { return }
+            completion(.success(data))
+        }
+        task.resume()
+    }
+    
+    enum NetworkError: Error {
+        case invalidURL
+        case invalidResponse
+        case statusCode(Int)
+    }
+}
---
+++ BooksInfo/Constants/URL.swift
@@ -0,0 +1,12 @@
+//
+//  URL.swift
+//  BooksInfo
+//
+//  Created by Сергей Матвеенко on 2.05.23.
+//
+
+import Foundation
+
+enum BooksApiURLs: String {
+    case booksInfoApiURL = "https://openlibrary.org/search.json?author=J.K.+Rowling&limit=5&offset=0"
+}
